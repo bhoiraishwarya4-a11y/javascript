909e84fc70c222c085cc4365f917fddea4536947
bhoiraishwarya4-a11y <bhoiraishwarya4@gmail.com>
2026-07-18 20:25:44 +0530

    initial commit

---
+++ variable-datatype.js
@@ -0,0 +1,60 @@
+// store your name
+let name = "aishwarya";
+console.log(name);
+
+// /store your age
+let age = 22;
+console.log(age);
+
+// // store your city
+let city = 'pune';
+console.log(city);
+
+// // change the city value there are 2 ways
+
+let city = "Pune";
+console.log(city);
+city = "Mumbai";
+console.log(city);
+
+// addition of two number
+let a=5;
+let b=10;
+let c=a+b;
+console.log("a + b = ",c);
+
+
+// datatype
+
+// 1.string
+
+let collegename="SPPU";
+console.log(" collegename = ",collegename);
+
+// 2.boolean
+let isstudent = true;
+console.log("isstudent = ",isstudent);
+
+// 3.Number
+let age = 22;
+console.log(" age = ",age);
+
+// 4.bigint
+let population=123456789n;
+console.log("population = ",population);
+
+// 5.undefined
+let phone ;
+console.log("phone = ",phone);
+
+// 6.null
+let result=null;
+console.log("result = ",result);
+
+// 7.symbol
+let id = Symbol("id");
+console.log(id);
+
+// typeof
+let y = null;
+console.log(typeof y);
