282f3d66c41645391fbbb51467bbbe115c6e157a
bhoiraishwarya4-a11y <bhoiraishwarya4@gmail.com>
2026-08-07 18:11:31 +0530

    updatetd

--- array.js
+++ array.js
@@ -1,136 +1,136 @@
-// //1.Create a program to reverse an array manually//
-// let arr=[10,20,30,40,50];
-// let reversearr=[];
-// for(i=arr.length-1;i>=0;i--){
-//     reversearr.push(arr[i])
-// }
-// console.log(reversearr);
+//1.Create a program to reverse an array manually//
+let arr=[10,20,30,40,50];
+let reversearr=[];
+for(i=arr.length-1;i>=0;i--){
+    reversearr.push(arr[i])
+}
+console.log(reversearr);
 
-// //2.Find the second-largest number in an array//
-// let arrr=[10,50,30,80,20]
-// let larget=-Infinity
-// let secondlarget=-Infinity
+//2.Find the second-largest number in an array//
+let arrr=[10,50,30,80,20]
+let larget=-Infinity
+let secondlarget=-Infinity
 
-// for(let i=0;i<arrr.length;i++){
-//     if(arrr[i]>larget){
-//         secondlarget=larget;
-//         larget=arrr[i]
-//     }else if(arrr[i]>secondlarget && arrr[i]!=larget)
-//         secondlarget=arr[i]
-// }
-// console.log(secondlarget)
-
-
-// //3.Merge two arrays without duplicates//
-// let arr1=[10,20,30,40]
-// let arr2=[30,40,50,60]
-// let mergeArr=[]
-
-// for (let i = 0; i < arr1.length; i++) {
-//     if (!mergeArr.includes(arr1[i])) {
-//         mergeArr.push(arr1[i]);
-//     }
-// }
-// for (let i = 0; i < arr2.length; i++) {
-//     if (!mergeArr.includes(arr2[i])) {
-//         mergeArr.push(arr2[i]);
-//     }
-// }
-
-// console.log(mergeArr);
-
-// // 4.Find Frequency of Each Element
-// let arr = [1,2,2,3,3,3,4];
-
-// let freq = {};
-
-// for(let item of arr){
-//     if(freq[item]){
-//         freq[item]++;
-//     }else{
-//         freq[item]=1;
-//     }
-// }
-
-// console.log(freq);
+for(let i=0;i<arrr.length;i++){
+    if(arrr[i]>larget){
+        secondlarget=larget;
+        larget=arrr[i]
+    }else if(arrr[i]>secondlarget && arrr[i]!=larget)
+        secondlarget=arr[i]
+}
+console.log(secondlarget)
 
 
-// //5.implement Manual version of includes() methods//
-// let arr3=[10,20,30,40,50];
-// let search=30;
-// let found=false;
+//3.Merge two arrays without duplicates//
+let arr1=[10,20,30,40]
+let arr2=[30,40,50,60]
+let mergeArr=[]
 
-// for(let i=0;i<arr3.length;i++){
-//     if(arr3[i]===search){
-//         found=true;
-//         break;
-//     }
-// }
-// if(found){
-//     console.log("found")
-// }else {
-//     console.log("not found")
-// }
+for (let i = 0; i < arr1.length; i++) {
+    if (!mergeArr.includes(arr1[i])) {
+        mergeArr.push(arr1[i]);
+    }
+}
+for (let i = 0; i < arr2.length; i++) {
+    if (!mergeArr.includes(arr2[i])) {
+        mergeArr.push(arr2[i]);
+    }
+}
+
+console.log(mergeArr);
+
+// 4.Find Frequency of Each Element
+let arr = [1,2,2,3,3,3,4];
+
+let freq = {};
+
+for(let item of arr){
+    if(freq[item]){
+        freq[item]++;
+    }else{
+        freq[item]=1;
+    }
+}
+
+console.log(freq);
 
 
-// //6.Shift Elements Left by One Position//
-// let number=[10,20,30,40,50]
-// let newnum=[]
-// for(i=1;i<number.length;i++){
-//     newnum.push(arr[i])
-// }
-// console.log(newnum)
+//5.implement Manual version of includes() methods//
+let arr3=[10,20,30,40,50];
+let search=30;
+let found=false;
 
-// // 7.Rotate Array Right by K Steps
-// let arr=[1,2,3,4,5];
-// let k=2;
+for(let i=0;i<arr3.length;i++){
+    if(arr3[i]===search){
+        found=true;
+        break;
+    }
+}
+if(found){
+    console.log("found")
+}else {
+    console.log("not found")
+}
 
-// k=k%arr.length;
 
-// for(let j=0;j<k;j++){
+//6.Shift Elements Left by One Position//
+let number=[10,20,30,40,50]
+let newnum=[]
+for(i=1;i<number.length;i++){
+    newnum.push(arr[i])
+}
+console.log(newnum)
 
-//     let last=arr[arr.length-1];
+// 7.Rotate Array Right by K Steps
+let arr=[1,2,3,4,5];
+let k=2;
 
-//     for(let i=arr.length-1;i>0;i--){
-//         arr[i]=arr[i-1];
-//     }
+k=k%arr.length;
 
-//     arr[0]=last;
-// }
+for(let j=0;j<k;j++){
 
-// console.log(arr);
+    let last=arr[arr.length-1];
 
-// // 8.Remove Duplicate Elements
+    for(let i=arr.length-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
 
-// let arr=[1,2,2,3,4,4,5];
+    arr[0]=last;
+}
 
-// let unique=[];
+console.log(arr);
 
-// for(let item of arr){
+// 8.Remove Duplicate Elements
 
-//     if(!unique.includes(item)){
-//         unique.push(item);
-//     }
+let arr=[1,2,2,3,4,4,5];
 
-// }
+let unique=[];
 
-// console.log(unique);
+for(let item of arr){
 
-// //9. 2D Array Multiplication Table (1–10)
-// let table=[];
+    if(!unique.includes(item)){
+        unique.push(item);
+    }
 
-// for(let i=1;i<=10;i++){
+}
 
-//     let row=[];
+console.log(unique);
 
-//     for(let j=1;j<=10;j++){
-//         row.push(i*j);
-//     }
+//9. 2D Array Multiplication Table (1–10)
+let table=[];
 
-//     table.push(row);
-// }
+for(let i=1;i<=10;i++){
 
-// console.log(table);
+    let row=[];
+
+    for(let j=1;j<=10;j++){
+        row.push(i*j);
+    }
+
+    table.push(row);
+}
+
+console.log(table);
 
 // 10.Intersection of Two Arrays
 let arr1=[1,2,3,4,5];
