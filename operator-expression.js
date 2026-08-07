b2323f5ac953035ec70aba12b3c054bbd5bd1f97
bhoiraishwarya4-a11y <bhoiraishwarya4@gmail.com>
2026-08-07 18:14:17 +0530

    added new

--- operator-expression.js
+++ operator-expression.js
@@ -1,225 +1,225 @@
 // arithmatic operators
 
-// addition,subtraction,multiplication,division,remainder,exponetional
+addition,subtraction,multiplication,division,remainder,exponetional
 
-// let num1 = Number(prompt("enter the number:"));
-// let num2 = Number(prompt("enter the number:"));
-// console.log("addition = ",num1+num2);
-// console.log("subtraction = ",num1-num2);
-// console.log("multiplication = ",num1*num2);
-// console.log("division = ",num1/num2);
-// console.log("remainder = ",num1%num2);
-// console.log("exponetional = ",num1**num2);
-
-
-// // increment,decrement
-// let num = Number(prompt("enter the number:"));
-// console.log("increment = ", ++num);
-// let num2 =  Number(prompt("enter the number:"));
-// console.log("decrement = ",--num2);
-
-// // reactangle area and perimeter find
-// let lenght=9;
-// let width=2;
-// let area=lenght*width;
-// console.log("area = ",area);
-// let perimeter=2*lenght+width;
-// console.log("perimeter",perimeter);
+let num1 = Number(prompt("enter the number:"));
+let num2 = Number(prompt("enter the number:"));
+console.log("addition = ",num1+num2);
+console.log("subtraction = ",num1-num2);
+console.log("multiplication = ",num1*num2);
+console.log("division = ",num1/num2);
+console.log("remainder = ",num1%num2);
+console.log("exponetional = ",num1**num2);
 
 
-// // find the average of 5 
-// let math=30;
-// let hindi=50;
-// let eng=68;
-// let sci=88;
-// let average=math+hindi+eng+sci/5;
-// console.log("average =",average);
+// increment,decrement
+let num = Number(prompt("enter the number:"));
+console.log("increment = ", ++num);
+let num2 =  Number(prompt("enter the number:"));
+console.log("decrement = ",--num2);
 
-// // find average by user number
-// let num1=Number(prompt("enter the first number"));
+// reactangle area and perimeter find
+let lenght=9;
+let width=2;
+let area=lenght*width;
+console.log("area = ",area);
+let perimeter=2*lenght+width;
+console.log("perimeter",perimeter);
 
-// let num2=Number(prompt("enter the second number"));
 
-// let num3=Number(prompt("enter the third number"));
+// find the average of 5 
+let math=30;
+let hindi=50;
+let eng=68;
+let sci=88;
+let average=math+hindi+eng+sci/5;
+console.log("average =",average);
 
-// let num4=Number(prompt("enter the fourth number"));
+// find average by user number
+let num1=Number(prompt("enter the first number"));
 
-// let num5=Number(prompt("enter the five number"));
-// console.log("average = ",num1+num2+num3+num4+num5/5);
+let num2=Number(prompt("enter the second number"));
+
+let num3=Number(prompt("enter the third number"));
+
+let num4=Number(prompt("enter the fourth number"));
+
+let num5=Number(prompt("enter the five number"));
+console.log("average = ",num1+num2+num3+num4+num5/5);
 
 // check even or odd by %
 
-// let Number=10;
-// if(Number%2 ==0){
-//     console.log("number is even")
-// }else{
-//     console.log("number is odd")
-// };
+let Number=10;
+if(Number%2 ==0){
+    console.log("number is even")
+}else{
+    console.log("number is odd")
+};
 
-// let num1=Number(prompt("enter the number:"));
-// console.log("num1 = ",num1);
-// console.log("num1%2 = ",num1%2);
-// if(num1%2 ==1){
-//     console.log("number is odd")
-// }else{
-//     console.log("number is even")
-// };
+let num1=Number(prompt("enter the number:"));
+console.log("num1 = ",num1);
+console.log("num1%2 = ",num1%2);
+if(num1%2 ==1){
+    console.log("number is odd")
+}else{
+    console.log("number is even")
+};
 
 
 
 // ASSIGNMENT OPERATOR
 
-// store 25 value in variable and print 
-// let b = 25;
-// console.log("b = ", b);
+store 25 value in variable and print 
+let b = 25;
+console.log("b = ", b);
 
-// // एका विद्यार्थ्याला 50 marks आहेत. त्याला 10 bonus marks मिळाले. Final marks print करा.
-// let marks = 50;
-// // marks += 10;
-// console.log("marks = ",marks=marks+10);
+// एका विद्यार्थ्याला 50 marks आहेत. त्याला 10 bonus marks मिळाले. Final marks print करा.
+let marks = 50;
+// marks += 10;
+console.log("marks = ",marks=marks+10);
 
 
-// // तुमच्या Wallet मध्ये ₹500 आहेत. तुम्ही ₹150 खर्च केले. आता किती पैसे उरले?
-// let money = 500;
-// money -= 150;
-// console.log("monet =",money);
+// तुमच्या Wallet मध्ये ₹500 आहेत. तुम्ही ₹150 खर्च केले. आता किती पैसे उरले?
+let money = 500;
+money -= 150;
+console.log("monet =",money);
 
-// // एका Box मध्ये 8 Chocolates आहेत. अशा 4 Boxes आहेत. Total Chocolates किती?
+// एका Box मध्ये 8 Chocolates आहेत. अशा 4 Boxes आहेत. Total Chocolates किती?
 
-// let chocolate=8;
-// chocolate*=4;
-// console.log("chocolate = ",chocolate);
+let chocolate=8;
+chocolate*=4;
+console.log("chocolate = ",chocolate);
 
-// // 100 Chocolates 4 मुलांमध्ये समान वाटा. प्रत्येकाला किती मिळतील?
-// let child=100;
-// child/=4;
-// console.log("child =",child);
+// 100 Chocolates 4 मुलांमध्ये समान वाटा. प्रत्येकाला किती मिळतील?
+let child=100;
+child/=4;
+console.log("child =",child);
 
-// // 17 ला 5 ने भाग दिल्यावर remainder किती येतो?
-// let divided=17;
-// divided %= 5;
-// console.log("moduls = ",divided);
+// 17 ला 5 ने भाग दिल्यावर remainder किती येतो?
+let divided=17;
+divided %= 5;
+console.log("moduls = ",divided);
 
-// // find square of 3
-// let square=3;
-// square**=2;
-// console.log("square = ",square);
+// find square of 3
+let square=3;
+square**=2;
+console.log("square = ",square);
 
-// COMPARISON OPERATOR
+COMPARISON OPERATOR
 
 
 // equal to ==
-// let a=50;
-// let b=50;
-// let c=a==b;
-// console.log(c);
+let a=50;
+let b=50;
+let c=a==b;
+console.log(c);
 
-// // grater than >
-// let x=60;
-// y=30;
-// c=x>y;
-// console.log(c);
+// grater than >
+let x=60;
+y=30;
+c=x>y;
+console.log(c);
 
-// // less than
-// let e=20;
-// let d=50;
-// console.log(e<d);
+// less than
+let e=20;
+let d=50;
+console.log(e<d);
 
-// let age=20;
-// f=age>=20;
-// console.log(f);
+let age=20;
+f=age>=20;
+console.log(f);
 
-// let g=10;
-// let h=40;
-// console.log(g!=h);
+let g=10;
+let h=40;
+console.log(g!=h);
 
-// let s="30";
-// let j="60";
-// console.log(s!==j);
+let s="30";
+let j="60";
+console.log(s!==j);
 
-// let o = 20;
-// let p = 10;
+let o = 20;
+let p = 10;
 
-// console.log(o + p > 25);
+console.log(o + p > 25);
 
-// let marks=90;
-// console.log(marks<=10);
+let marks=90;
+console.log(marks<=10);
 
 
-// // LOGICAL OPERATOR
+// LOGICAL OPERATOR
 
-// let a=56;
-// console.log(++a)
-// let hasid=true;
-// let c=a>50&&hasid
-// console.log(c);
+let a=56;
+console.log(++a)
+let hasid=true;
+let c=a>50&&hasid
+console.log(c);
 
-// let marks=35;
-// if(marks>=29){
-//     console.log("you can enter next class")
-// }
+let marks=35;
+if(marks>=29){
+    console.log("you can enter next class")
+}
 
-// let age=20;
-// if(age>=18){
-//     console.log("you are eligible")
-// }else{
-//     console.log("you are not eligible")
-// };
+let age=20;
+if(age>=18){
+    console.log("you are eligible")
+}else{
+    console.log("you are not eligible")
+};
 
-// let num1=5;
-// if(num1%2==0){
-//     console.log("even");
-// }else{
-//     console.log("odd")
-// };
+let num1=5;
+if(num1%2==0){
+    console.log("even");
+}else{
+    console.log("odd")
+};
 
 // arithmetic operator
 
-// let num1=parseFloat(prompt("enter the first number:"));
-// let num2=parseFloat(prompt("enter the second number:"));
+let num1=parseFloat(prompt("enter the first number:"));
+let num2=parseFloat(prompt("enter the second number:"));
 
-// console.log("addition:",num1+num2);
+console.log("addition:",num1+num2);
 
-// console.log("subtraction:",num1-num2);
+console.log("subtraction:",num1-num2);
 
-// console.log("multiplication:",num1*num2);
+console.log("multiplication:",num1*num2);
 
-// console.log("division:",num1/num2);
+console.log("division:",num1/num2);
 
-// console.log("remainder:",num1%num2);
+console.log("remainder:",num1%num2);
 
-// console.log("exponent:",num1**num2);
+console.log("exponent:",num1**num2);
 
 // comparison operator
 
-// let num3=parseFloat(prompt("enter the first number:"));
-// let num4=parseFloat(prompt("enter the second number:"));
+let num3=parseFloat(prompt("enter the first number:"));
+let num4=parseFloat(prompt("enter the second number:"));
 
-// console.log("equal to = ",num3==num4);
-// console.log("not equal to =",num3!=num4);
+console.log("equal to = ",num3==num4);
+console.log("not equal to =",num3!=num4);
 
-// console.log("strict equal to =",num3===num4);
-// console.log("strict not equal to =",num3!==num4);
+console.log("strict equal to =",num3===num4);
+console.log("strict not equal to =",num3!==num4);
 
-// console.log("greater than =",num3>num4);
-// console.log("less than =",num3<num4);
+console.log("greater than =",num3>num4);
+console.log("less than =",num3<num4);
 
-// console.log("greater than equal to =",num3>=num4);
-// console.log("less than equal to =",num3<=num4);
-
-
-
-//logical operator
-
-// let mathscore=parseFloat(prompt("enter the math score:"));
-// let sciscore=parseFloat(prompt("enter the science score:"));
-
-// console.log("scolarship eligiblity:",mathscore>=60 && sciscore>=70);
-// console.log("scolarship eligiblity:",mathscore>=60 || sciscore<=70);
-// console.log("scolarship eligiblity:",!(mathscore==sciscore))
+console.log("greater than equal to =",num3>=num4);
+console.log("less than equal to =",num3<=num4);
 
 
-//asssingnment operator
+
+// logical operator
+
+let mathscore=parseFloat(prompt("enter the math score:"));
+let sciscore=parseFloat(prompt("enter the science score:"));
+
+console.log("scolarship eligiblity:",mathscore>=60 && sciscore>=70);
+console.log("scolarship eligiblity:",mathscore>=60 || sciscore<=70);
+console.log("scolarship eligiblity:",!(mathscore==sciscore))
+
+
+// asssingnment operator
 
 let x=10;
 console.log("initial value of x =",x);
